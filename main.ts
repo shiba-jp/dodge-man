2ae409e9c52ae9255812a95cb50e7d93d5c8bd15
shiba-jp <63838451+shiba-jp@users.noreply.github.com>
2020-12-29 13:32:10 +0900

    Add speed up function.

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
-function 敵生成 () {
+function generateEnemy () {
     Enemy = game.createSprite(randint(0, 4), 0)
     basic.pause(500)
 }
-function Player移動 () {
+function initialize () {
+    generateEnemy()
+    Player = game.createSprite(2, 4)
+    game.setScore(0)
+    game.setLife(3)
+    startTime = input.runningTime()
+    basic.pause(500)
+}
+function getGameSpeed () {
+    currentTime = input.runningTime()
+    elapsedTime = currentTime - startTime
+    gameSpeed = 500
+    if (elapsedTime >= 10000 && elapsedTime < 20000) {
+        gameSpeed = 200
+    }
+    if (elapsedTime >= 20000) {
+        gameSpeed = 100
+    }
+    return gameSpeed
+}
+function movePlayer () {
     if (input.acceleration(Dimension.X) > 50) {
         Player.change(LedSpriteProperty.X, 1)
     }
@@ -10,28 +30,25 @@ function Player移動 () {
         Player.change(LedSpriteProperty.X, -1)
     }
 }
-function 初期化 () {
-    敵生成()
-    Player = game.createSprite(2, 4)
-    game.setScore(0)
-    game.setLife(3)
-    basic.pause(500)
-}
+let gameSpeed = 0
+let elapsedTime = 0
+let currentTime = 0
+let startTime = 0
 let Player: game.LedSprite = null
 let Enemy: game.LedSprite = null
-初期化()
+initialize()
 basic.forever(function () {
     while (game.isRunning()) {
-        Player移動()
+        movePlayer()
         Enemy.change(LedSpriteProperty.Y, 1)
-        basic.pause(200)
+        basic.pause(getGameSpeed())
         if (Player.isTouching(Enemy)) {
             game.removeLife(1)
         }
         if (Enemy.get(LedSpriteProperty.Y) == 4) {
             Enemy.delete()
             game.addScore(1)
-            敵生成()
+            generateEnemy()
         }
     }
 })
